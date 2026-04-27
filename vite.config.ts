b5c41ce8c65cb23da3c3dd4b86767f9094ffc951
chibi <mac.dollarr@hotmail.com>
2026-04-26 22:08:04 -0300

    such

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: "/gwg/",   // 👈 THIS IS REQUIRED
+    base: "/",   // 👈 THIS IS REQUIRED
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
